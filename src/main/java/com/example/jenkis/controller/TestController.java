c4be45248249768ebf73f5a4dfb4f4702b941c8e
minsu <zmfjtnl1@naver.com>
2024-02-13 14:48:23 +0900

    commit

--- src/main/java/com/example/jenkis/controller/TestController.java
+++ src/main/java/com/example/jenkis/controller/TestController.java
@@ -5,9 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    
+
     @GetMapping("/test")
     public String test() {
         return "test";
+    }
+
+    @GetMapping("/test2")
+    public String test2() {
+        return "test2";
     }
 }
