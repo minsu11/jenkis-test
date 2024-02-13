9534c9276b81485eb1965c9d28890c708c333c13
minsu <zmfjtnl1@naver.com>
2024-02-13 13:17:17 +0900

    first commit

---
+++ src/main/java/com/example/jenkis/JenkisApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkisApplication.class, args);
+	}
+
+}
